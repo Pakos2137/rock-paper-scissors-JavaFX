917f2353b1b35bc7314ef12e2db7083bf5d89fbc
Pakos2137 <michal.pakos@Onet.pl>
2023-11-07 22:25:00 +0100

    Initial commit

---
+++ src/main/java/com/example/rockpaperscissorsjavafx/Main.java
@@ -0,0 +1,24 @@
+package com.example.rockpaperscissorsjavafx;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MainScreen.fxml"));
+        StackPane stackPane = loader.load();
+        Scene scene = new Scene(stackPane,400,400);
+        primaryStage.setTitle("Kamień Papier Nożyce");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
